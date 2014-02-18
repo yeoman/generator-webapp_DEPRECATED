4da82e38cbb2303db2094eabe6f69c0850141954
Camille Reynders <camille.reynders@gmail.com>
2014-02-18 12:19:32 +0100

    Replace load-grunt-tasks with jit-grunt

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -10,7 +10,7 @@
 module.exports = function (grunt) {
 
     // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
+    require('jit-grunt')(grunt);
 
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
