648f011c234c1b4910cdf30eb8521f3ecd1a2406
Harry Moreno <morenoh149@gmail.com>
2014-03-26 03:37:00 -0700

    removed quotes on keys

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -387,16 +387,16 @@ module.exports = function (grunt) {
         // reference in your app
         modernizr: {
             dist: {
-                'devFile': '<%= yeoman.app %>/bower_components/modernizr/modernizr.js',
-                'outputFile': '<%= yeoman.dist %>/bower_components/modernizr/modernizr.js',
-                'files': {
-                    'src' : [
+                devFile: '<%= yeoman.app %>/bower_components/modernizr/modernizr.js',
+                outputFile: '<%= yeoman.dist %>/bower_components/modernizr/modernizr.js',
+                files: {
+                    src : [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
                         '!<%= yeoman.dist %>/scripts/vendor/*'
                     ]
                 },
-                'uglify': true
+                uglify: true
             }
         },<% } %>
 
