52b133c01931c62ed72a38032254c088b8f6977c
unknown <atrachtman@35XMSX1-9030.dealertire.com>
2015-07-06 11:42:01 -0400

    reverting change to Gruntfile.js to fix broken task in generated webapp

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -192,7 +192,7 @@ module.exports = function (grunt) {
         sourceMap: true,
         sourceMapEmbed: true,
         sourceMapContents: true,
-        includePaths: ['.']
+        includePaths: ['bower_components']
       },
       dist: {
         files: [{
